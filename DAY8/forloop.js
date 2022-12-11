9e317b36240cfbbcd2cc92dc134351295052d424
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2022-12-11 20:03:26 +0530

    For Loop

---
+++ DAY8/forloop.js
@@ -0,0 +1,20 @@
+/*  loops are used for repeating/for iterating  'n' no of times
+loops are two types entry controlled, exit controlled
+                            |                 |
+                        for, while         do while
+for loop is known as entry controlled loop which means it checks the condition first then executed */
+
+// syntax for forloop is : 
+/* for (initializationStatement; testExpression; updateStatement)
+{
+    // statements inside the body of loop
+}
+ */
+
+// example of for loop to execute '2' table
+ 
+for( let q=1; q<=false; q++)
+ {
+console.log(`2 x ${q}= ${q*2}`)
+ }
+ 
