8213a0138aab8bd9bd7f41ac8690bda23fe36282
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2022-12-11 20:11:46 +0530

    Switch Statement

---
+++ DAY9/switch.js
@@ -0,0 +1,15 @@
+/* The JavaScript switch statement is used in decision making.
+The switch statement evaluates an expression and executes the corresponding body that matches the expression's result.
+The syntax of the switch statement is:
+
+switch(variable/expression) {
+    case value1:  
+        // body of case 1
+        break;
+
+    case value2:  
+        // body of case 2
+        break;
+        default:
+            // body of default
+    } */
