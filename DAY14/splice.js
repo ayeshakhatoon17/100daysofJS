2981cb2c17d50b4eee68f569ac7ce3c7f0c93ca6
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2022-12-12 20:30:41 +0530

    splice.js

---
+++ DAY14/splice.js
@@ -0,0 +1,12 @@
+const myStates = [
+    "telangana",
+    "delhi",
+    "odisha",
+    "karnataka",
+    "telangana"
+]
+/* var updated = myStates.splice(0, 2) // 0 is from where to start  and 2 is how many elements
+console.log(updated) */
+
+var updated = myStates.splice(2, 2)
+console.log(updated)
