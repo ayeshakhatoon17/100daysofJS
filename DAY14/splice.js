a26dc101fe719070432c6f24b4bd71e0910a4129
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2022-12-13 22:37:37 +0530

    splice.js

--- DAY14/splice.js
+++ DAY14/splice.js
@@ -5,8 +5,8 @@ const myStates = [
     "karnataka",
     "telangana"
 ]
-/* var updated = myStates.splice(0, 2) // 0 is from where to start  and 2 is how many elements
-console.log(updated) */
+ /* var updated = myStates.splice(0, 2) // 0 is from where to start  and 2 is how many elements
+console.log(updated)  */ // it reutrns ['telanagana',' delhi']
 
-var updated = myStates.splice(2, 2)
-console.log(updated)
+/* var updated = myStates.splice(2, 2)//it is going to return from 2 and it also returns more 1  element total 2 as we gave 2 in the second parameter
+console.log(updated) */
