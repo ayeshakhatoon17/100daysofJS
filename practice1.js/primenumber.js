f4877ba22e458050e3d0ca17266856fe98008cfc
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2022-12-18 08:36:15 +0530

    Prime number example

--- practice1.js/primenumber.js
+++ practice1.js/primenumber.js
@@ -2,21 +2,25 @@
 // prime number starts from 2
 
 
-let num = 3; //taking value 10 and checking if it's prime or not
-var isPrime = true; //assuming the given no is prime first
+let num = 4; //taking value 10 and checking if it's prime or not
+var isPrime = true; //assuming the given nunmber is prime first
 
 for(let i = 2; i<num; i++) //prime 2 se start hora so i =2, we want to check if number is prime or not till num only so i <num
 {
-    if(num % i == 0)//if num divides with i which can be anything and gives reminder 0 then it's not prime coz it is divisible by that no
+    if(num % i == 0)//if num divides with i which can be anything and gives reminder 0 then it's not prime 
 {
   isPrime = false; // no is not a prime so we are reurning isPrime as false
 }
 }
-if (isPrime == true){
+if (isPrime == true){ // if  isPrime is true then the given no is prime and it prints num is a prime
 
  console.log(`${num} is a prime number`)
 }
-else {
-console.log(`${num} is n0t a prime number`)
+else { // if isPrime is false then it prints given number is not a prime
+console.log(`${num} is n0t a prime number`) 
 
-}
+} 
+
+
+
+
